f980b75cdb94e653f82c72cd8218ec2eb7406fcc
Jorge Oliveira <kaatancontos@gmail.com>
2022-08-15 20:35:29 -0300

    1508

--- javascript/index.js
+++ javascript/index.js
@@ -4,32 +4,41 @@ class Menu extends HTMLElement {
   }
   connectedCallback() {
     this.innerHTML = `
-<nav class="js-nav">
-  <ul>
-    <li><a href="../index.html">Início</a></li>
-    <li><a href="../Informatica/informatica.html">Informática</a></li>
-    <li class="js-drop-button">
-      <a>Programação <i class="seta"></i></a>
-      <div class="js-drop-content">
-        <a href="../Web Design/webdesign.html">Web Design</a>
-        <hr>
-        <a>em construção...</a>
-      </div>
-    </li>
-    <li class="right"><a href="../Sobre/sobre.html">Sobre</a></li>
-  </ul>
-  <style>
-    .seta {
-      border: solid #ddd;
-      border-width: 0 3px 3px 0;
-      display: inline-block;
-      padding: 2px;
-      transform: rotate(45deg);
-      -webkit-transform: rotate(45deg);
-      margin: auto 5px auto 5px;
-    }
-  </style>
-</nav>
+    <div class="js-nav">
+    <ul>
+      <li tabindex="1"><a href="https://www.jorgesouza.com.br/">Início</a></li>
+      <li class="js-drop-button" tabindex="2">
+        <a>Aprendizado <i class="seta"></i></a>
+        <div class="js-drop-content">
+          <a href="https://www.jorgesouza.com.br/informatica/intro">Informática</a>
+          <a href="#">Design Gráfico</a>
+          <hr>
+          <pre><small>Desenvolvimento Web:</small></pre>
+          <a>1. Web Design</a>
+          <a>2. Javascript</a>
+          <a>3. Aplicações Web e APIs</a>
+          <hr>
+          <pre><small>Programação:</small></pre>
+          <a>1. Desktop</a>
+          <a>2. Móvel</a>
+          <a>3. Gráficos e Multimídia</a>
+          <a>4. Jogos</a>
+        </div>
+      </li>
+      <li tabindex="3"><a>Apostilas</a></li>
+    </ul>
+    <style>
+      .seta {
+        border: solid #ddd;
+        border-width: 0 3px 3px 0;
+        display: inline-block;
+        padding: 2px;
+        transform: rotate(45deg);
+        -webkit-transform: rotate(45deg);
+        margin: auto 5px auto 5px;
+      }
+    </style>
+  </div>
     `;
   }
 }
@@ -59,54 +68,44 @@ class Footer extends HTMLElement {
   }
   connectedCallback() {
     this.innerHTML = `
-        <div class="js-footer">
-        <table>
-          <tr>
-            <td>
-              <tit>Manuais e Apostilas</tit>
-            </td>
-            <td>
-              <tit>Aprendizado On-Line</tit>
-            </td>
-            <td>
-              <tit>Contato</tit>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <par onclick="window.location.replace('#')">Informática Básica e Avançada</par>
-              <par onclick="window.location.replace('#')">Design Gráfico</par>
-              <par onclick="window.location.replace('#')">Web Design (HTML e CSS)</par>
-              <par onclick="window.location.replace('#')">Javascript (Front-End e Back-End)</par>
-              <par onclick="window.location.replace('#')">Programação de Gráficos e Multimídia</par>
-              <par onclick="window.location.replace('#')">Programação para PC Desktop</par>
-              <par onclick="window.location.replace('#')">Desenvolvimento de Apps Móveis</par>
-              <par onclick="window.location.replace('#')">Desenvolvimento de Jogos</par>
-            </td>
-            <td>
-              <par onclick="window.location.replace('../Informatica/informatica.html')">Informática Básica e Avançada</par>
-              <par onclick="window.location.replace('#')">Design Gráfico</par>
-              <par onclick="window.location.replace('../Web Design/webdesign.html')">Web Design (HTML e CSS)</par>
-              <par onclick="window.location.replace('#')">Javascript (Front-End e Back-End)</par>
-              <par onclick="window.location.replace('#')">Programação de Gráficos e Multimídia</par>
-              <par onclick="window.location.replace('#')">Programação para PC Desktop</par>
-              <par onclick="window.location.replace('#')">Desenvolvimento de Apps Móveis</par>
-              <par onclick="window.location.replace('#')">Desenvolvimento de Jogos</par>
-            </td>
-            <td>
-              <par onclick="window.open('mailto:jorge.sos777@gmail.com')">Gmail</par>
-              <par onclick="window.open('https://wa.me/5577991161892')">Whatsapp</par>
-              <par onclick="window.open('https://join.skype.com/invite/uMbQf0utKfUz')">Skype</par>
-              <par onclick="window.open('https://www.facebook.com/artesgraficaseprojetos')">Facebook</par>
-              <par onclick="window.open('https://www.instagram.com/jorgeoliveiraonline/')">Instagram</par>
-              <par onclick="window.open('https://www.workana.com/freelancer/175498bc00eeda4731ad4044f609f5a5')">Workana (Freelancer)</par>
-            </td>
-          </tr>
-        </table>
-        <hr>
-        <img src="../files/logo_amarela.svg" style="pointer-events: none; vertical-align: middle; border: none; background-color: transparent; width: 40px; display: inline-block; margin: auto;"/>
-        <tit style="vertical-align: middle; text-align: center; padding: 20px; display: inline-block;">Copyright © 2022 - Jorge Souza Oliveira dos Santos</tit>
-      </div>
+    <div class="js-footer">
+    <div style="min-width: 300px;">
+      <tit>Aprendizado On-Line</tit>
+      <par onclick="window.location = 'https://www.jorgesouza.com.br/informatica/intro'">Informática Básica e Avançada</par>
+      <par onclick="window.location = '#'">Design Gráfico</par>
+      <par onclick="window.location = '#'">Web Design (HTML e CSS)</par>
+      <par onclick="window.location = '#'">Javascript (Front-End e Back-End)</par>
+      <par onclick="window.location = '#'">Programação de Gráficos e Multimídia</par>
+      <par onclick="window.location = '#'">Programação para PC Desktop</par>
+      <par onclick="window.location = '#'">Desenvolvimento de Apps Móveis</par>
+      <par onclick="window.location = '#'">Desenvolvimento de Jogos</par>
+    </div>
+    <div style="min-width: 300px;">
+      <tit>Manuais e Apostilas</tit>
+      <par onclick="window.location = '#'">Informática Básica e Avançada</par>
+      <par onclick="window.location = '#'">Design Gráfico</par>
+      <par onclick="window.location = '#'">Web Design (HTML e CSS)</par>
+      <par onclick="window.location = '#'">Javascript (Front-End e Back-End)</par>
+      <par onclick="window.location = '#'">Programação de Gráficos e Multimídia</par>
+      <par onclick="window.location = '#'">Programação para PC Desktop</par>
+      <par onclick="window.location = '#'">Desenvolvimento de Apps Móveis</par>
+      <par onclick="window.location = '#'">Desenvolvimento de Jogos</par>
+    </div>
+    <div style="min-width: 300px;">
+      <tit>Contato</tit>
+      <par onclick="window.open('mailto:jorge.sos777@gmail.com')">Gmail</par>
+      <par onclick="window.open('https://wa.me/5577991161892')">Whatsapp</par>
+      <par onclick="window.open('https://join.skype.com/invite/uMbQf0utKfUz')">Skype</par>
+      <par onclick="window.open('https://www.facebook.com/artesgraficaseprojetos')">Facebook</par>
+      <par onclick="window.open('https://www.instagram.com/jorgeoliveiraonline/')">Instagram</par>
+      <par onclick="window.open('https://www.workana.com/freelancer/175498bc00eeda4731ad4044f609f5a5')">Workana (Freelancer)</par>
+    </div>
+    <div style="min-width: 90%;">
+      <hr style="width: 90%; margin: 25px auto 25px auto">
+      <img src="../files/logo_amarela.svg" style="pointer-events: none; border: none; background-color: transparent; width: 40px; display: block; margin: auto; align-self: center;"/>
+      <tit style="align-self: center; text-align: center; padding: 20px; display: block;">Copyright © 2022 - Jorge Souza Oliveira dos Santos</tit>
+    </div>
+  </div>
     `;
   }
 }

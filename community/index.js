177fee53edc3a3b6e577221c3c6b9fd5825bcfb2
Jorge Oliveira <kaatancontos@gmail.com>
2025-03-21 12:55:21 -0300

    21032025_1

--- community/index.js
+++ community/index.js
@@ -1,8 +1,11 @@
-const App = {
+const Community = {
+    Posts: [],
+    User: {},
     Main: async () => {
         TOPBAR.innerHTML = "";
-        //
+
         // Add icons to the TOPBAR:
+
         const span = document.createElement("span");
         span.classList = "icon";
         span.style.height = "30px";
@@ -15,14 +18,13 @@ const App = {
             span.classList.add('active');
             window.location = '/';
         };
-        span.onmouseover = () => Tooltip.Tooltip("Sair", span);
+        span.onmouseover = () => Tooltip.Tooltip("Sair da sala", span);
 
         const img = document.createElement("img");
         img.src = "https://kaatan.azurewebsites.net/files/exit.svg";
         img.style.height = "24px";
         span.appendChild(img);
         TOPBAR.appendChild(span);
-        //TOPBAR.style.justifyContent = "center";
 
         const span2 = document.createElement("span");
         span2.textContent = "Comunidade | Jorge Souza";
@@ -35,9 +37,331 @@ const App = {
         span2.style.userSelect = "none";
         TOPBAR.appendChild(span2);
         console.log("App Started");
+
+        // Load user data:
+
+        SideMenuContent.ShowProfileSection = true;
+        const u = localStorage.getItem("myprofile");
+        if (u) {
+            const _ = JSON.parse(u);
+        } else {
+            const server = "https://kaatan-server.azurewebsites.net/u/getnewuser";
+            const u = new Model.User();
+            u.Id = "1";
+            u.Username = "DF8-EF95";
+            u.Name = "Anonimous";
+            u.Description = "New Kaatan User";
+            u.Thumbnail = "https://kaatan.azurewebsites.net/files/default.svg";
+            u.Token = "f90sd8f908sd09f0sd0f980s9d8f09";
+            
+
+            u.Id = "1";
+            u.Username = "DF8-EF95";
+            u.Name = "Anonimous";
+            u.Description = "New Kaatan User";
+            u.Email = "meu@mail.com";
+            u.Genre = "male";
+            u.Thumbnail = "https://kaatan.azurewebsites.net/files/default.svg";
+            u.Token = "f90sd8f908sd09f0sd0f980s9d8f09";
+            u.State = {};
+            SideMenuContent.ProfileSection = {
+                ProfilePic: u.Thumbnail,
+                ProfileName: u.Name,
+                ProfileUsername: u.Username,
+                Action: () => Renderer.Components.MyProfilePage(
+                    u.Id,
+                    u.Username,
+                    u.Name,
+                    u.Description,
+                    u.Thumbnail,
+                    u.Genre,
+                    u.Email,
+                    u.State,
+                    (userData => {
+                        console.log(userData);
+                    })),
+            };
+        };
+
+        // Load posts:
+        await Community.PostBox();
     },
-    //
-    _: async () => { },
+    MyProfile: async () => { },
+    // Render posts
+    PostBox: async () => {
+        // Carrega os posts:
+        await Community.LoadPosts();
+        APPVIEW.innerHTML = "";
+        Renderer.Load("posts-view", APPVIEW).then(async x => {
+            const postView = document.getElementById("posts-view");
+            // Renderiza os posts:
+            Community.Posts.forEach(post => {
+                Renderer.Components.PostBox(post, postView, Community.UserProfile, bodyClickCB, likeClickCB, Community.CommentBox, saveClickCB);
+            });
+
+            // Renderiza a caixa de texto:
+            postView.appendChild(document.createElement("horizontal-divider"));
+            postView.appendChild(Community.EditBox(true, post));
+            postView.scrollTo({ left: 0, top: (Number.MAX_SAFE_INTEGER * -1), behavior: "smooth" });
+            function post() {
+                console.log("Posted!");
+            }
+        });
+    },
+    // Render comments in a post:
+    CommentBox: async (id) => {
+        //
+        const comments = Community.Posts.find(x => x.PostId == id).Comments;
+        console.log(Community.Posts.find(x => x.PostId == id).Username);
+
+        //APPVIEW.innerHTML = "";
+        Renderer.Load("comment-view", APPVIEW).then(x => {
+            const cmmView = document.getElementById("comment-view");
+
+            comments.forEach(cmm => {
+                Renderer.Components.CommentBox(cmm, cmmView, bodyClickCB, Community.UserProfile, replyClickCB, likeClickCB);
+                document.getElementById("comment-container-close").onclick = async () => {
+                    APPVIEW.removeChild(document.getElementById('comment-container'));
+                    await Community.PostBox();
+                };
+            });
+
+            // Renderiza a caixa de texto:
+            cmmView.insertBefore(document.createElement("horizontal-divider"), cmmView.firstChild);
+            cmmView.insertBefore(Community.EditBox(false, comment), cmmView.firstChild);
+            cmmView.scrollTo({ left: 0, top: Number.MAX_SAFE_INTEGER, behavior: "smooth" });
+            function comment() {
+                console.log("Commented!");
+            }
+        });
+    },
+    // Load posts:
+    LoadPosts: async () => {
+        // Temporary local retrive:
+        Community.Posts = JSON.parse(sessionStorage.getItem("posts"));
+    },
+    // Render the textbox to edit posts and comments:
+    EditBox: (post = true, callback = () => { }) => {
+        const card = document.createElement("card-big");
+        card.classList.add("no-elastic");
+        card.classList.add("no-hover");
+
+        // Cabeçalho:
+        if (post) {
+            const header = document.createElement("div");
+            header.classList.add("post-header");
+            const headerContent = `
+                <div class="post-header">
+                    <span class="post-profilepic">
+                    <img src="https://kaatan.azurewebsites.net/avatars/f/14.jpg"></span>
+                    <span class="sender">Usuário 14</span>
+                </div>`;
+            header.innerHTML = headerContent;
+            card.appendChild(header);
+            card.appendChild(document.createElement("horizontal-divider"));
+        }
+
+        // Caixa de texto:
+        const edit = document.createElement("textarea");
+        edit.classList.add("acacia-input");
+        edit.style.minHeight = "100px";
+        edit.style.resize = "none";
+        edit.style.width = "100%";
+        edit.placeholder = "Digite seu comentário...";
+        edit.maxLength = "1000";
+
+        edit.oninput = e => {
+            edit.style.height = 'auto'; // Reseta a altura
+            edit.style.height = `${edit.scrollHeight}px`;
+        };
+
+        const postBtn = document.createElement("button-squared");
+        postBtn.textContent = post ? "Postar" : "Comentar";
+        postBtn.onclick = callback;
+        card.appendChild(edit);
+        card.appendChild(document.createElement("horizontal-divider"));
+        card.append(postBtn);
+        return card;
+    },
+    MyProfile: async () => {
+        document.getElementById("leftbar-my-profile-btn").onclick = () => Renderer.Components.MyProfilePage(
+            "AAA-0000",
+            "anonimo@aaa0000",
+            "Jorge Souza Oliveira dos Santos",
+            "Novo usuário Kaatan. Sempre experimentando novas experiências!",
+            "https://kaatan.azurewebsites.net/avatars/f/1.jpg",
+            "female",
+            "meu@mail.com",
+            {
+                "Insignia": "https://kaatan.azurewebsites.net/insignias/insignia.jpg",
+                "DaysStreak": 0,
+                "RegisterTime": null,
+                "LastAccessTime": 1718308735837,
+                "MailVerified": false,
+                "UserVerified": false,
+                "Points": 1000,
+                "Stars": 0,
+                "RankName": "Visitante",
+                "RankLevel": 0,
+            },
+            (userData => {
+                console.log(userData)
+            })
+        );
+    },
+    UserProfile: async (id) => {
+        // Get user profile:
+        console.log(id);
+        //
+        return;
+        //
+        document.getElementById("leftbar-user-profile-btn").onclick = () => Renderer.Components.UserProfilePage(
+            "AAA-0000",
+            "anonimo@aaa0000",
+            "Jorge Souza Oliveira dos Santos",
+            "Novo usuário Kaatan. Sempre experimentando novas experiências!",
+            "https://kaatan.azurewebsites.net/avatars/f/1.jpg",
+            0,
+            {
+                "Dias consecutivos: ": "0",
+                "Registrado em: ": "Não registrado",
+                "Último acesso: ": new Date(1718308735837).toLocaleString(),
+                "Pontos: ": "1000",
+                "Nota: ": "★★★★☆",
+                "Visitas no perfil: ": "1",
+                "Nível": "Visitante"
+            }
+        );
+    },
+    _: async () => {
+        //
+    },
 };
 
-window.App = App;
+window.Community = Community;
+
+const Model = {
+    UserData: class {
+        Id;
+        Username;
+        Name;
+        Description;
+        Thumbnail;
+        Genre;
+        Email;
+        Token;
+        State = new Object();
+    },
+    User: class {
+        Id;
+        Username;
+        Name;
+        Description;
+        Thumbnail;
+        Token;
+    },
+    RoomData: class { },
+    Room: class {
+        Id;
+        Username;
+        Name;
+        Description;
+        Thumbnail;
+        Users = {};
+    },
+    Message: class {
+        MessageId;
+        Time;
+        Username;
+        Name;
+        Receiver;
+        Thumbnail;
+        Type;
+        Content;
+        Upvotes;
+        Self;
+    },
+    Post: class {
+        PostId;
+        Time;
+        Username;
+        Name;
+        Thumbnail;
+        Type;
+        Content;
+        Upvotes;
+        Comments;
+    },
+    Comment: class {
+        CommentId;
+        Time;
+        Username;
+        Name;
+        Content;
+        Upvotes;
+        Thumbnail;
+    },
+};
+
+// Initializes posts and userdata
+function resetPosts() {
+    // Posts:
+    const posts = [];
+    const comments = [];
+
+    for (let i = 1; i < 15; i++) {
+        const c1 = new Model.Comment();
+        c1.CommentId = `${i}`;
+        c1.Content = "Êpa, isso é um comentário!"
+        c1.Name = `Usuário ${i}`;
+        c1.Thumbnail = `https://kaatan.azurewebsites.net/avatars/f/${i}.jpg`;
+        c1.Time = Date.now();
+        c1.Upvotes = 10 + i;
+        c1.Username = `XV${i}-PL55`;
+        comments.push(c1); comments.push(c1);
+
+        const p1 = new Model.Post();
+        p1.PostId = `${i}`;
+        p1.Content = "Êpa, isso é uma postagem!"
+        p1.Name = `Usuário ${i}`;
+        p1.Thumbnail = `https://kaatan.azurewebsites.net/avatars/f/${i}.jpg`;
+        p1.Time = Date.now();
+        p1.Type = "Text";
+        p1.Upvotes = 10 + (i * 2);
+        p1.Username = `XV${i}-PL55`;
+        p1.Comments = comments;
+        posts.push(p1);
+    }
+    sessionStorage.setItem("posts", JSON.stringify(posts));
+
+    // User:
+    {
+        "AAA-0000",
+            "anonimoaaa0000",
+            "Anônimo",
+            "Novo usuário Kaatan",
+            "https://kaatan.azurewebsites.net/avatars/f/1.jpg",
+            "female",
+            "meu@mail.com",
+        {
+            "Insignia": "https://kaatan.azurewebsites.net/files/insignia.jpg",
+            "DaysStreak": 0,
+            "RegisterTime": null,
+            "LastAccessTime": 1718308735837,
+            "MailVerified": false,
+            "UserVerified": false,
+            "Points": 1000,
+            "Stars": 0,
+            "RankName": "Visitante",
+            "RankLevel": 0
+        }
+    }
+}
+
+// Temporary callbacks
+const bodyClickCB = (id) => console.log("bodyClickCB: ", id);
+const replyClickCB = (id) => console.log("replyClickCB: ", id);
+const likeClickCB = (id) => console.log("likeClickCB: ", id);
+const saveClickCB = (id) => console.log("saveClickCB: ", id);
+
+resetPosts();
--- jorge.js
+++ jorge.js
@@ -32,7 +32,7 @@ const App = {
                 action: () => window.open('https://www.kaatan.com.br')
             },
             {
-                icon: "/files/chat.svg",
+                icon: "https://kaatan.azurewebsites.net/files/comunity.svg",
                 title: "Comunidade",
                 action: App.Community
             },
@@ -129,8 +129,7 @@ const App = {
                 <img src="files/outlook.svg" onmouseover="Tooltip.Tooltip('E-mail', this)" onclick="window.open('mailto:jorge.sos777@outlook.com')">
                 <img src="files/whatsapp.svg" onmouseover="Tooltip.Tooltip('Whatsapp', this)" onclick="window.open('https://wa.me/5577999030420')">
                 <img src="files/amazon.svg" onmouseover="Tooltip.Tooltip('Amazon', this)" onclick="window.open('https://amazon.com.br/kindle-dbs/entity/author?asin=B0CM13195T')">
-                <img src="files/instagram.svg" onmouseover="Tooltip.Tooltip('Instagram', this)" onclick="window.open('https://www.instagram.com/jorgesouzaonline/')">
-                <img src="files/chat.svg" onmouseover="Tooltip.Tooltip('Enviar mensagem', this)" onclick="App.Community()">
+                <img src="https://kaatan.azurewebsites.net/files/comunity.svg" onmouseover="Tooltip.Tooltip('Comunidade', this)" onclick="App.Community()">
             </grid-row>
 
             <horizontal-divider></horizontal-divider>
@@ -147,7 +146,6 @@ const App = {
                 <text-link class="footer-link" style="padding: 0; font-size: small; background-color: transparent;" onclick="window.location = '/'">início</text-link>
                 <text-link class="footer-link" style="padding: 0; font-size: small; background-color: transparent;" onclick="App.Blog()">blog</text-link>
                 <text-link class="footer-link" style="padding: 0; font-size: small; background-color: transparent;" onclick="App.Learn()">cursos</text-link>
-                <text-link class="footer-link" style="padding: 0; font-size: small; background-color: transparent;" onclick="App.Books()">livros</text-link>
                 <text-link class="footer-link" style="padding: 0; font-size: small; background-color: transparent;" onclick="window.open('https://www.kaatan.com.br')">kaatan</text-link>
                 <text-link class="footer-link" style="padding: 0; font-size: small; background-color: transparent;" onclick="App.Digital()">educação digital</text-link>
                 <text-link class="footer-link" style="padding: 0; font-size: small; background-color: transparent;" onclick="App.Community()">comunidade</text-link>
@@ -361,42 +359,6 @@ const App = {
         //community();
         disqus();
     },
-    // Checkout:
-    Checkout: (id) => {
-        const appContainer = document.createElement("app-container");
-
-        const cursoContainer = document.createElement("card-big");
-        cursoContainer.innerHTML = `
-            <text-heading>Finalizar compra</text-heading>
-            <horizontal-divider></horizontal-divider>
-            <text-paragraph>
-                Para finalizar a compra, entre em contato pelo formulário abaixo.
-                O preenchimento do formulário é indispensável, por meio dele você informará qual e-book deseja adquirir e também por ele receberá o arquivo em PDF. Qualquer dúvida ou sugestão, fique à vontade para entrar em contato pelo meios disponíveis neste site.
-            </text-paragraph>
-            <horizontal-divider></horizontal-divider>
-            <!-- <iframe hiddens src="https://docs.google.com/forms/d/e/1FAIpQLSdgI7zlnpYJ-fw3DJZLvzhWTsjXWNbY2i-oGIRNLVevEKGBPQ/viewform?embedded=true" width="100%" height="1000px" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe> -->
-            <iframe id="form" hiddens src="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAANAASV1IhlUN0VNWE5FQVBPODRQVkNNSDlPWjRERU1ROS4u" width="100%" height="1500px" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>
-        `;
-        cursoContainer.classList.add("no-elastic");
-        cursoContainer.id = id;
-        appContainer.appendChild(cursoContainer);
-
-        appContainer.appendChild(document.createElement("horizontal-divider"));
-
-        APPVIEW.innerHTML = "";
-        APPVIEW.appendChild(appContainer);
-        App.Footer();
-        document.getElementById("app-view").scrollTo({ top: 0, left: 0, behavior: "smooth" });
-        setInterval(function () {
-            var currentUrl = document.getElementById("form").contentWindow.location.href;
-            console.log(currentUrl);
-            if (currentUrl !== lastUrl) {
-                console.log('A URL do iframe mudou!');
-                lastUrl = currentUrl;
-                // Aqui você pode verificar se a nova URL indica que o formulário foi enviado
-            }
-        }, 1000);
-    },
     // IA & ML:
     IA: {
         Learn: async () => {
@@ -556,160 +518,6 @@ const App = {
         },
     },
     // Cursos:
-    /*
-    Cad: () => {
-        const curso = App.Cursos.find(cursos => cursos.id == "cad");
-        const appContainer = document.createElement("app-container");
-        appContainer.appendChild(App.Course(
-            curso["id"],
-            curso["titulo"],
-            curso["descrição"],
-            curso["capa"],
-            curso["a-venda"],
-            curso["downloads"],
-            curso["ebook"],
-            curso["kindle"],
-            curso["preview"]
-        ));
-        appContainer.appendChild(document.createElement("horizontal-divider"));
-        APPVIEW.innerHTML = "";
-        APPVIEW.appendChild(appContainer);
-        App.Footer();
-    },
-    "3d": () => {
-        const curso = App.Cursos.find(cursos => cursos.id == "3d");
-        const appContainer = document.createElement("app-container");
-        appContainer.appendChild(App.Course(
-            curso["id"],
-            curso["titulo"],
-            curso["descrição"],
-            curso["capa"],
-            curso["a-venda"],
-            curso["downloads"],
-            curso["ebook"],
-            curso["kindle"],
-            curso["preview"]
-        ));
-        appContainer.appendChild(document.createElement("horizontal-divider"));
-        APPVIEW.innerHTML = "";
-        APPVIEW.appendChild(appContainer);
-        App.Footer();
-    },
-    Info: () => {
-        const curso = App.Cursos.find(cursos => cursos.id == "info");
-        const appContainer = document.createElement("app-container");
-        appContainer.appendChild(App.Course(
-            curso["id"],
-            curso["titulo"],
-            curso["descrição"],
-            curso["capa"],
-            curso["a-venda"],
-            curso["downloads"],
-            curso["ebook"],
-            curso["kindle"],
-            curso["preview"]
-        ));
-        appContainer.appendChild(document.createElement("horizontal-divider"));
-        APPVIEW.innerHTML = "";
-        APPVIEW.appendChild(appContainer);
-        App.Footer();
-    },
-    Design: () => {
-        const curso = App.Cursos.find(cursos => cursos.id == "design");
-        const appContainer = document.createElement("app-container");
-        appContainer.appendChild(App.Course(
-            curso["id"],
-            curso["titulo"],
-            curso["descrição"],
-            curso["capa"],
-            curso["a-venda"],
-            curso["downloads"],
-            curso["ebook"],
-            curso["kindle"],
-            curso["preview"]
-        ));
-        appContainer.appendChild(document.createElement("horizontal-divider"));
-        APPVIEW.innerHTML = "";
-        APPVIEW.appendChild(appContainer);
-        App.Footer();
-    },
-    Web: () => {
-        const curso = App.Cursos.find(cursos => cursos.id == "web");
-        const appContainer = document.createElement("app-container");
-        appContainer.appendChild(App.Course(
-            curso["id"],
-            curso["titulo"],
-            curso["descrição"],
-            curso["capa"],
-            curso["a-venda"],
-            curso["downloads"],
-            curso["ebook"],
-            curso["kindle"],
-            curso["preview"]
-        ));
-        appContainer.appendChild(document.createElement("horizontal-divider"));
-        APPVIEW.innerHTML = "";
-        APPVIEW.appendChild(appContainer);
-        App.Footer();
-    },
-    JS: () => {
-        const curso = App.Cursos.find(cursos => cursos.id == "js");
-        const appContainer = document.createElement("app-container");
-        appContainer.appendChild(App.Course(
-            curso["id"],
-            curso["titulo"],
-            curso["descrição"],
-            curso["capa"],
-            curso["a-venda"],
-            curso["downloads"],
-            curso["ebook"],
-            curso["kindle"],
-            curso["preview"]
-        ));
-        appContainer.appendChild(document.createElement("horizontal-divider"));
-        APPVIEW.innerHTML = "";
-        APPVIEW.appendChild(appContainer);
-        App.Footer();
-    },
-    App: () => {
-        const curso = App.Cursos.find(cursos => cursos.id == "app");
-        const appContainer = document.createElement("app-container");
-        appContainer.appendChild(App.Course(
-            curso["id"],
-            curso["titulo"],
-            curso["descrição"],
-            curso["capa"],
-            curso["a-venda"],
-            curso["downloads"],
-            curso["ebook"],
-            curso["kindle"],
-            curso["preview"]
-        ));
-        appContainer.appendChild(document.createElement("horizontal-divider"));
-        APPVIEW.innerHTML = "";
-        APPVIEW.appendChild(appContainer);
-        App.Footer();
-    },
-    IA: () => {
-        const curso = App.Cursos.find(cursos => cursos.id == "ia");
-        const appContainer = document.createElement("app-container");
-        appContainer.appendChild(App.Course(
-            curso["id"],
-            curso["titulo"],
-            curso["descrição"],
-            curso["capa"],
-            curso["a-venda"],
-            curso["downloads"],
-            curso["ebook"],
-            curso["kindle"],
-            curso["preview"]
-        ));
-        appContainer.appendChild(document.createElement("horizontal-divider"));
-        APPVIEW.innerHTML = "";
-        APPVIEW.appendChild(appContainer);
-        App.Footer();
-    },
-    */
     Digital: () => {
         Modal.ConfirmAction("Curso de Segurança Digital para a Família", "Este é um curso interativo, divertido e, com certificado de conclusão! Então o que está esperando? Quer aprender a excitante experiência da tecnologia de forma segura e consciente? Clique no botão abaixo e comece sua jornada!", () => window.open("/digital"));
     },

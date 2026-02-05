85ae7e868559efd4c03530aa43de73c61d66ee09
Jorge Oliveira <kaatancontos@gmail.com>
2026-02-05 00:26:30 -0300

    05022026_4

--- jorge.js
+++ jorge.js
@@ -528,13 +528,25 @@ const App = {
         Modal.ConfirmAction("Curso de Segurança Digital para a Família", "Este é um curso interativo, divertido e, com certificado de conclusão! Então o que está esperando? Quer aprender a excitante experiência da tecnologia de forma segura e consciente? Clique no botão abaixo e comece sua jornada!", () => window.open("/digital"));
     },
     Projects: () => {
+        // Galeria
         const galleryContainer = document.createElement("card-big");
         galleryContainer.classList.add("no-hover");
         galleryContainer.classList.add("no-elastic");
         const appContainer = document.createElement("app-container");
         appContainer.appendChild(galleryContainer);
         APPVIEW.innerHTML = "";
-        APPVIEW.appendChild(appContainer);
+        
+        // Título:
+        const title = document.createElement("h2");
+        
+        // Descrição:
+        const description = document.createElement("text-paragraph");
+
+        const headerContainer = document.createElement("card-big");
+        headerContainer.classList.add("no-hover");
+        headerContainer.classList.add("no-elastic");
+        headerContainer.appendChild(title);
+        headerContainer.appendChild(description);
 
         const sources = [
             "https://www.jorgesouza.com.br/projects files/1.jpg",
@@ -563,6 +575,10 @@ const App = {
             "https://www.jorgesouza.com.br/projects files/24.jpg"
         ];
         const gallery = new Gallery("myGallery", sources, galleryContainer);
+
+        // Adicionar elementos:
+        APPVIEW.appendChild(headerContainer);
+        APPVIEW.appendChild(appContainer);
     },
     Contact: () => {
         window.open('https://wa.me/5577999030420');
